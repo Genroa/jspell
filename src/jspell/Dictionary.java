494c8a0cb388dab2b71a0ead579c4890a9e5bafa
genro <genro@172.22.246.170>
2016-06-02 11:42:56 +0200

    Loading is done

--- src/jspell/Dictionary.java
+++ src/jspell/Dictionary.java
@@ -19,7 +19,39 @@ public class Dictionary
 	{
 		try(Scanner s = new Scanner(f))
 		{
-			s.useDelimiter("\\{Punct}");
+			s.useDelimiter("[\\p{Punct}\\p{Space}]");
+			while(s.hasNext())
+			{
+				String w = s.next();
+				if(!w.isEmpty())
+				{
+					w = w.toLowerCase();
+					Double occurences = words.get(w);
+					if(occurences == null)
+					{
+						words.put(w, 1.0);
+					}
+					else
+					{
+						words.put(w, occurences+1);
+					}
+				}
+			}
 		}
+		
+		System.out.println(words.size()+" mots chargés.");
+	}
+	
+	private double getProbability(String s)
+	{
+		Double occurence = words.get(s);
+		if(occurence == null) return 0;
+		return occurence/words.size();
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		System.out.println("UNIT TEST FOR DICTIONARY");
+		Dictionary d = new Dictionary(new File("germinal.txt"));
 	}
 }
