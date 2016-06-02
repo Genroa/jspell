ca8073a84d74cf59d6ade155e494b71d39bbe203
genro <genro@172.22.246.170>
2016-06-02 11:23:08 +0200

    Working on Dictionary

--- src/jspell/Dictionary.java
+++ src/jspell/Dictionary.java
@@ -1,6 +1,25 @@
 package jspell;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Dictionary 
 {
-
+	private final HashMap<String, Double> words;
+	
+	public Dictionary(File f) throws FileNotFoundException
+	{
+		words = new HashMap<>();
+		buildWordsList(f);
+	}
+	
+	private void buildWordsList(File f) throws FileNotFoundException
+	{
+		try(Scanner s = new Scanner(f))
+		{
+			s.useDelimiter("\\{Punct}");
+		}
+	}
 }
